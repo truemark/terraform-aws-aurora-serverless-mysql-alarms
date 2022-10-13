fb522c92876c90e8d0ad6237e53c8d0ecf54d4b4
Lisa Koivu <lisamarie.koivu@gmail.com>
2022-10-13 15:26:08 -0400

    Changing defaults for iops

--- thresholds.tf
+++ thresholds.tf
@@ -8,9 +8,9 @@ locals {
     TotalBackupStorageBilledThreshold                 = min(max(var.total_backup_storage_billed_threshold, 0), 1000000000000) # 1TB In bytes.
     VolumeBytesUsedEvaluationPeriods                  = min(max(var.volume_bytes_used_evaluation_periods, 1), 100)
     VolumeBytesUsedThreshold                          = min(max(var.volume_bytes_used_threshold, 0), 1000000000000) # 1TB In bytes.
-    VolumeReadIOPsEvaluationPeriods                   = min(max(var.volume_read_iops_evaluation_periods, 1), 100)
+    VolumeReadIOPsEvaluationPeriods                   = min(max(var.volume_read_iops_evaluation_periods, 1), 10000)
     VolumeReadIOPsThreshold                           = max(var.volume_read_iops_threshold, 0)
-    VolumeWriteIOPsEvaluationPeriods                  = min(max(var.volume_write_iops_evaluation_periods, 1), 100)
+    VolumeWriteIOPsEvaluationPeriods                  = min(max(var.volume_write_iops_evaluation_periods, 1), 10000)
     VolumeWriteIOPsThreshold                          = max(var.volume_write_iops_threshold, 0)
   }
 }
